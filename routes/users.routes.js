fc2f5d6b1d69893e0c17b7deac3156da245b896b
roi bublil <roibub@post.bgu.ac.il>
2026-05-03 19:22:16 +0300

    fixing work flow

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
+const authorize = require("../middleware/auth");
 const { getAllUsers, getUserById, createUser, updateUser, deleteUser} = require('../controllers/users.controller');
+
 // if u asked for GET /api/users
 router.get('/', getAllUsers);
 // if u asked for GET /api/users/:id
 router.get('/:id', getUserById);
-// if u asked for POST /api/users
-router.post('/', createUser);
-
-// if u asked for PUT /api/users/:id
-router.put('/:id', updateUser);
-// if u asked for DELETE /api/users/:id
-router.delete('/:id', deleteUser);
+// if u asked for POST /api/users only for admin
+router.post('/' , authorize(['admin']), createUser);
+// if u asked for PUT /api/users/:id only for admin
+router.put('/:id' , authorize(['admin']), updateUser);
+// if u asked for DELETE /api/users/:id only for admin
+router.delete('/:id' , authorize(['admin']), deleteUser);
 module.exports = router;
+
