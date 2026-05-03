70af867ea650d13e9702ab0a0d1ab4b3af7d139b
roi bublil <roibub@post.bgu.ac.il>
2026-05-03 17:12:19 +0300

    fixing work flow

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAllUsers, getUserById, createUser } = require('../controllers/users.controller');
+const { getAllUsers, getUserById, createUser, updateUser} = require('../controllers/users.controller');
 // if u asked for GET /api/users
 router.get('/', getAllUsers);
 // if u asked for GET /api/users/:id
 router.get('/:id', getUserById);
 // if u asked for POST /api/users
 router.post('/', createUser);
+// if u asked for PUT /api/users
+router.put('/:id', updateUser);
 
 module.exports = router;
